88ec51894358dfc9e89123174eec9e0988344b47
Gentille <gisagentille55@gmail.com>
2019-08-05 20:49:50 +0200

    adding total amount of piza

--- js/scripts.js
+++ js/scripts.js
@@ -19,23 +19,26 @@ function getSelectedItem(sel) {
       
   }
   
-  function checkedbutton(smRadioBtn){
+  function checkedutton(smRadioBtn){
     var smRadioBtn = document.querySelectorAll(".sm-radio-btn")
     var sommes = 0;
     var allItem =""
     for(var i = 0; i < smRadioBtn.length; i++){
         if (smRadioBtn[i].checked == true) {
             var nextS = smRadioBtn[i].nextSibling.nextSibling;
-            var item = smRadioBtn[i].nextSibling;
-            //console.log(item);
-            var  price = nextS.firstChild.nextSibling;
-            var prix = price.innerText;
+            // var item = smRadioBtn[i].nextSibling;
+            var nextS2 = nextS.nextSibling.nextSibling.firstChild.nextSibling;
             
-            var prs = parseInt(prix);
-            var btn = document.createElement("P");
-            btn.innerText = item;
-            var result = document.querySelector(".checkout");
-            result.innerHTML = btn;
+            // console.log(nextS);
+            var item = nextS.innerText;
+            console.log(typeof(item));
+            var  price = nextS2;
+            var getPrice = price.innerText;
+            
+            var prs = parseInt(getPrice);
+            console.log(typeof(prs));
+            allItem += item +":"+getPrice+"+  "
+            
             sommes += prs;
             checkOut(item, prs, sommes, allItem)
         }
@@ -45,7 +48,7 @@ function getSelectedItem(sel) {
         //console.log(sommes);
 } 
 // JSON.stringify(label)
-function checkOut(label, amount, total){
+function checkOut(label, amount, total, allItem){
     console.log(label, amount);
 
     console.log(`Total: ${total}`)
@@ -54,5 +57,60 @@ function checkOut(label, amount, total){
         e.preventDefault();
         var result = document.querySelector(".checkout")
         result.innerText = allItem;
-    })
+        // document.querySelector(".total").innerText = total;
+        var parag = document.createElement("P");
+        parag.innerText = total;
+        result.append(parag);
+
+        var deliv = document.createElement("BUTTON");
+        deliv.innerText = "Delivary";
+        deliv.type = "button";
+        deliv.className = "delivar";
+        result.append(deliv);
+
+        computeResult(total, allItem);
+    });
 }
+
+function computeResult(total, allItem){
+    document.querySelector(".delivar").addEventListener('click', e=>{
+        e.preventDefault();
+        // var txt;
+        var r = confirm("Can we help you for your pizza delivery to your appropriate place?");
+
+        if (r == true) {
+            var addr = prompt("Enter your location address");
+            if(addr){
+                var delivCharge = 0;
+                var conf = confirm("you will be charged 2000rw for delivary");
+                if(conf == true){
+                    delivCharge = 2000;
+                    var grandTotal = total + delivCharge;
+                    var summary = "Selected items: " + allItem + " delivery charge: " + delivCharge + " Total amount to be paid: " + grandTotal;
+                    var clientAddress = "  to the address: " +addr;
+
+                    var finalResult = document.createElement("DIV");
+                    finalResult.className = "final-result";
+                    finalResult.style.cssFloat = "right";
+                    finalResult.style.fontFamily = "";
+                    finalResult.append(summary);
+                    finalResult.append(clientAddress);
+                    // result.append(finalResult);
+                    document.querySelector(".summary").append(finalResult);
+
+                }else{
+                    alert("you have concelled the delivery service");
+                }
+            }else{
+                alert("you must enter your location address to get our delivery service");
+            }
+        } else {
+        alert("You pressed Cancel!");
+        }
+    });
+}
+
+
+// var item = smRadioBtn[i].nextSibling;
+
+//             var item = smRadioBtn[i].nextSibling;
